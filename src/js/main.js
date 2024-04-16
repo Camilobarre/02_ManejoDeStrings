f95e97f06aaa7be99c6ec26bba7936dc81dde4ca
Camilobarre <barrecami@gmail.com>
2024-04-16 10:47:27 -0500

    Metodos para extraer carácteres

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,19 @@
+//Declaracion de Strings
 const option1= "Hola Mundo"
 const option2= String("Hola Mundo")
 const option3= new String("Hola Mundo")
 
-console.info(option1)
-console.info(option2)
-console.info(option3)
+// console.info(option1)
+// console.info(option2)
+// console.info(option3)
+
+//Extraccion de datos
+const message="Este es un mensaje para pruebas"
+document.write(message)
+
+//Extrar la cantidad de caracteres
+console.info(message.length)
+
+//Extraer un caracter
+console.info(message[1])
+console.info(message.charAt(1))
