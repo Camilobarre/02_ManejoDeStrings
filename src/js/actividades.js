ec1ccbdb71585efcd7e3d16468777b2653193a9b
Camilobarre <barrecami@gmail.com>
2024-04-19 07:41:13 -0500

    Se continua con los ejercicios de Strings

--- src/js/actividades.js
+++ src/js/actividades.js
@@ -1,7 +1,7 @@
 //Ejercicio 1
-let num1=5;
-let num2=2;
-const suma=num1+num2;
+let num1 = 5;
+let num2 = 2;
+const suma = num1 + num2;
 console.info(suma)
 
 //Ejercicio 2
@@ -30,7 +30,7 @@ console.log("La posición del substring 'ndo' es:", substringPosition2);
 console.info("Concatenación:", simpleString1.concat(" ").concat(simpleString2))
 
 //Ejercicio 8
-console.info("Utilizando (+): " + simpleString1+" "+simpleString2)
+console.info("Utilizando (+): " + simpleString1 + " " + simpleString2)
 
 //Ejercicio 9
 console.info(`${simpleString1} ${simpleString2}`)
@@ -42,13 +42,13 @@ console.info(stringWithSpaces1.trimStart())
 console.info(stringWithSpaces2.trimEnd())
 
 //Ejercicio 11
-console.info(simpleString1.replace("o","i"))
+console.info(simpleString1.replace("o", "i"))
 
 //Ejercico 12
 console.info(simpleString2.slice(3))
 
 //Ejercicio 13
-console.info(simpleString2.substring(2,4))
+console.info(simpleString2.substring(2, 4))
 
 //Ejercicio 14
 console.info(simpleString1.repeat(2))
@@ -65,16 +65,84 @@ console.info(simpleString1.toLowerCase())
 
 //Ejercicio 18
 let variable = true;
-console.info(typeof(variable));
+console.info(typeof (variable));
 
 //Ejercicio 19
-let array=[1,2,3,4,5,6];
+let array = [1, 2, 3, 4, 5, 6];
 console.info(array.length)
 
 //Ejercicio 20
 let object = {
-    propiedad1: "Camilo",
-    propiedad2: "Barreneche",
-    propiedad3: "Escobar"
-  };
-  console.info(object.propiedad2);
+  propiedad1: "Camilo",
+  propiedad2: "Barreneche",
+  propiedad3: "Escobar"
+};
+console.info(object.propiedad2);
+
+//Ejercicio 21
+let nulo = null;
+console.info(typeof (nulo));
+
+//Ejercicio 22
+let indefinido
+console.info(typeof (indefinido));
+
+//Ejercicio 23
+let decimal = 12.5;
+console.info(typeof (decimal));
+
+//Ejercicio 24
+let negativo = -32;
+console.info(typeof (negativo));
+
+//Ejercicio 25
+let palabra = "Casa";
+console.info(indexOf("a"))
+
+//Ejercicio 26
+let palabra2 = "Javascript";
+console.info(palabra2.includes("Script"));
+
+//Ejercicio 27
+let palabraUnoConcat = "Hola";
+let palabraDosConcat = "Mundo";
+console.info(palabraUnoConcat.concat(" ").concat(palabraDosConcat))
+
+//Ejercicio 28
+let palabraUnoSuma = "Hola";
+let palabraDosSuma = "Mundo";
+console.info(palabraUnoSuma + " " + palabraDosSuma)
+
+//Ejercicio 29
+let palabraUnoTemplate = "Hola";
+let palabraDosTemplate = "Mundo";
+console.info(`${palabraUnoTemplate} ${palabraDosTemplate}`);
+
+//Ejercicio 30
+let cadenaConEspacios = " Ejemplo con espacios ";
+cadenaConEspacios = cadenaConEspacios.trimStart().trimEnd();
+console.info(cadenaConEspacios);
+
+//Ejercicio 31
+let oracion = "El cielo es azul";
+let nuevaOracion = oracion.replace("azul", "rojo");
+console.info(nuevaOracion);
+
+//Ejercicio 32
+let otraOracion = "Programación";
+let ultimosCuatro = otraOracion.slice(-4);
+console.info(ultimosCuatro);
+
+//Ejercicio 33
+let nuevaPalabra = "JavaScript";
+let subcadena = nuevaPalabra.substring(2);
+console.info(subcadena);
+
+//Ejercicio 34
+let unaPalabraMas= "Hola"
+console.info(unaPalabraMas.repeat(3))
+
+//Ejercicio 35
+let otraOracionMas = "Esto es una oración de ejemplo";
+let arrayPalabras = otraOracionMas.split(" ");
+console.info(arrayPalabras);
